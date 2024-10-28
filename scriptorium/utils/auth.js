22148ebfeb654dff6702dad3d1d50375260b7017
Harry Xu <harryxcz@gmail.com>
2024-10-28 19:09:10 -0400

    update token verification

--- scriptorium/utils/auth.js
+++ scriptorium/utils/auth.js
@@ -9,10 +9,12 @@ export function generateToken(user) {
 }
 
 export function verifyToken(token) {
-    if (!token) {
+    if (!token?.startsWith('Bearer ')) { // If it doesn't stat with 'Bearer '
         return null;
     }
+
     else {
+        token = token.split(' ')[1];
         try {
             return jwt.verify(token, JWT_SECRET);
         }
