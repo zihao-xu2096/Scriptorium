56c7df39b4701b520f6f805a1e3955368ba7cb70
Harry Xu <harryxcz@gmail.com>
2024-10-29 23:16:16 -0400

    add profile and refresh apis

--- scriptorium/pages/api/profile.js
+++ scriptorium/pages/api/profile.js
@@ -1,15 +1,26 @@
-import { verifyToken } from '../../utils/auth';
+import { prisma } from '../../prisma/prisma';
+import { verifyAccessToken } from '../../utils/auth';
+
 
 async function handler(req, res) {
     if (req.method === 'GET') {
-        const user = verifyToken(req.headers.authorization)
+        const decoded = verifyAccessToken(req.headers.authorization) // Checks Token timestamp
+        // If token is invalid, return 401
 
-        if (!user) {
+        if (!decoded) { 
+            //TODO: Try to generate new access token, if refresh token is valid
             return res.status(401).json({
-                message: 'User Unauthorized'
+                message: 'Token Expired' // FRONTEND responsible for sending another request to create new access token
             })
-        } // TODO: Try to generate new access token???
+        }
 
+        const user = await prisma.user.findUnique({
+            where: {
+                id: decoded.id
+            }
+        })
+
+        // TODO: Consider checking user exists (should exist since logged in)
         return res.status(200).json({
             firstName: user.firstName,
             lastName: user.lastName,
