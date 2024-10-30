e530b15cb36edc954ee04b8116cdb5cfc3409577
Harry Xu <harryxcz@gmail.com>
2024-10-29 23:16:31 -0400

    fix token storing

--- scriptorium/utils/auth.js
+++ scriptorium/utils/auth.js
@@ -1,14 +1,30 @@
 import jwt from 'jsonwebtoken';
 const JWT_SECRET = 'adfjasjdflasja89d(&(*&d9u1df'
 const JWT_EXPIRES_IN = '1h'
+const JWT_REFRESH_SECRET = 'adfasdfasdfasdfasdfasdf'
+const JWT_REFRESH_EXPIRES_IN = '7d'
 
-export function generateToken(user) {
-    return jwt.sign(user, JWT_SECRET, {
+export function generateAccessToken(user) {
+    const payload ={
+        id: user.id,
+        email: user.email,
+    }
+    return jwt.sign(payload, JWT_SECRET, {
         expiresIn: JWT_EXPIRES_IN,
     })
 }
 
-export function verifyToken(token) {
+export function generateRefreshToken(user) {
+    const payload ={
+        id: user.id,
+        email: user.email,
+    }
+    return jwt.sign(payload, JWT_REFRESH_SECRET, {
+        expiresIn: JWT_REFRESH_EXPIRES_IN,
+    })
+} // TODO: Instead of signing User obj sign user id
+
+export function verifyAccessToken(token) {
     if (!token?.startsWith('Bearer ')) { // If it doesn't stat with 'Bearer '
         return null;
     }
@@ -21,5 +37,14 @@ export function verifyToken(token) {
         catch (error) {
             return null;
         }
+    }
+}
+
+export function verifyRefreshToken(token) {
+    try {
+        return jwt.verify(token, JWT_REFRESH_SECRET);
+    }
+    catch (error) {
+        return null;
     }
 }
