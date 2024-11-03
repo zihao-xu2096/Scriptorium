3babbfa859b7850e44cf1286b77657ff9bcfd5cd
Peter Won <pw@babblefm.com>
2024-11-03 12:01:22 -0500

    fix email validation

--- scriptorium/pages/api/signup.js
+++ scriptorium/pages/api/signup.js
@@ -9,7 +9,7 @@ async function validateEmailFormat(email) {
     // Validate email format
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return false
+        return False
     }
     return True
 }
