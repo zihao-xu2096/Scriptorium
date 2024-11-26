e937ac6216b36a32d87bfda77697d3562df90b1e
Harry Xu <harryxcz@gmail.com>
2024-11-26 17:23:25 -0500

    add login URL for unauthorized user

--- PP2/pages/profile.tsx
+++ PP2/pages/profile.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -68,6 +69,9 @@ export default function Profile() {
       <div className="flex items-center justify-center min-h-screen bg-gray-900">
         <div className="w-full max-w-md p-8 space-y-6 bg-gray-800 rounded-lg shadow-lg">
           <h2 className="text-3xl font-bold text-center text-white">Not Authorized/Not Logged In</h2>
+          <p className="text-center text-sm text-gray-400">
+            <Link href="/login" className="text-indigo-500 hover:text-indigo-400 underline">Login here</Link>.
+          </p>          
         </div>
       </div>
     );
